079f3af3ffda9eb41b3a65c84e6bf73d4ce199cf
liguanghao <liguanghao@viatomtech.com>
2022-08-13 22:12:48 +0800

    asdfasdf

--- app/src/main/java/com/vaca/pc300/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/vaca/pc300/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.vaca.pc300.databinding.FragmentHomeBinding
+import com.vaca.pc300.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
