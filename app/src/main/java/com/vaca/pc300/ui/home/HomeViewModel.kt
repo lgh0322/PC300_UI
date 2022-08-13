435b66e044db9361b9e229b438875eaca9f39acb
liguanghao <liguanghao@viatomtech.com>
2022-08-13 22:02:43 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/pc300/ui/home/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.vaca.pc300.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+
+
+    fun changeText(a:String){
+        _text.postValue("gaga")
+
+    }
+}
