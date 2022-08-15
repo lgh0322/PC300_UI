83bed3dd9a27bef5ce95ca916d010eafe0cdcd61
liguanghao <liguanghao@viatomtech.com>
2022-08-15 16:44:41 +0800

    sdgcfbhasdfxcv

---
+++ app/src/main/java/com/vaca/pc300/LPM311SelectActivity.kt
@@ -0,0 +1,28 @@
+package com.vaca.pc300
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.vaca.pc300.databinding.ActivityLpm311AboutBinding
+import com.vaca.pc300.databinding.ActivityLpm311HistoryDetailBinding
+import com.vaca.pc300.databinding.ActivityMainBinding
+import com.vaca.pc300.databinding.ActivityTempDetailBinding
+
+class LPM311SelectActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLpm311AboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding =ActivityLpm311AboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
