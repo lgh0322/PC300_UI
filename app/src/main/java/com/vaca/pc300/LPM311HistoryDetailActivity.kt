6b549093a79a2739aefa79e8518ce0e3d0f702fa
liguanghao <liguanghao@viatomtech.com>
2022-08-15 16:09:11 +0800

    dsgfdgfdggvghdfggjdsfggfh

---
+++ app/src/main/java/com/vaca/pc300/LPM311HistoryDetailActivity.kt
@@ -0,0 +1,27 @@
+package com.vaca.pc300
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.vaca.pc300.databinding.ActivityLpm311HistoryDetailBinding
+import com.vaca.pc300.databinding.ActivityMainBinding
+import com.vaca.pc300.databinding.ActivityTempDetailBinding
+
+class LPM311HistoryDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLpm311HistoryDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding =ActivityLpm311HistoryDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
