9e65cee09fb04c51a50077880d4998d9b76faf48
liguanghao <liguanghao@viatomtech.com>
2022-08-15 17:17:35 +0800

    sdgcfbhasdfxcvdfgcvnghjvgjj

--- app/src/main/java/com/vaca/pc300/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/vaca/pc300/ui/history/HistoryFragment.kt
@@ -5,12 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vaca.pc300.databinding.FragmentHistoryBinding
-import com.vaca.pc300.ui.history.adapter.LPM311HistoryAdapter
 import com.vaca.pc300.ui.history.adapter.PoctorTopAdapter
 
 class HistoryFragment : Fragment() {
@@ -34,18 +32,19 @@ class HistoryFragment : Fragment() {
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        topAdapter= PoctorTopAdapter(requireContext())
+        topAdapter = PoctorTopAdapter(requireContext())
 
 
 
-        binding.topView.layoutManager = object :   LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false) {
+        binding.topView.layoutManager =
+            object : LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false) {
 
 
-            override fun canScrollHorizontally(): Boolean {
-                return false
+                override fun canScrollHorizontally(): Boolean {
+                    return false
+                }
             }
-        }
-        binding.topView.adapter =topAdapter
+        binding.topView.adapter = topAdapter
 
 
 
