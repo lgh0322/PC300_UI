54b357eb53201cad994460dd47fc31f012f46287
liguanghao <liguanghao@viatomtech.com>
2022-08-15 14:10:53 +0800

    dfgfddfgftghfghasdfgfgvbvbnghj

--- app/src/main/java/com/vaca/pc300/PC300BPDetailActivity.kt
+++ app/src/main/java/com/vaca/pc300/PC300BPDetailActivity.kt
@@ -7,21 +7,19 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.vaca.pc300.databinding.ActivityBpDetailBinding
 import com.vaca.pc300.databinding.ActivityMainBinding
 
 class PC300BPDetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityBpDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityBpDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-        navView.itemIconTintList=null
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)
+
     }
 }
