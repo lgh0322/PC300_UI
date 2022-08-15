1d3e0a474ea5e634ec3307d64622b3c52718764e
liguanghao <liguanghao@viatomtech.com>
2022-08-15 10:01:14 +0800

    bnmfhgdffggfsdgdfsgfdg

--- app/src/main/java/com/vaca/pc300/ui/history/adapter/PC300HistoryLeftAdapter.kt
+++ app/src/main/java/com/vaca/pc300/ui/history/adapter/PC300HistoryLeftAdapter.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.vaca.pc300.R
 
 
 class PC300HistoryLeftAdapter(var context: Context) : RecyclerView.Adapter<PC300HistoryLeftAdapter.ViewHolder>() {
+
+    companion object{
+        var currentSelect=0;
+    }
 
     private val mData: MutableList<String> = ArrayList()
     private val mInflater: LayoutInflater = LayoutInflater.from(context)
@@ -32,7 +38,11 @@ class PC300HistoryLeftAdapter(var context: Context) : RecyclerView.Adapter<PC300
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.name.text=name[position]
-
+        if(position== currentSelect){
+            holder.name.setTextColor(ContextCompat.getColor(context,R.color.color_111827))
+        }else{
+            holder.name.setTextColor(ContextCompat.getColor(context,R.color.color_9CA3AF))
+        }
     }
 
 
@@ -42,6 +52,14 @@ class PC300HistoryLeftAdapter(var context: Context) : RecyclerView.Adapter<PC300
 
     inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
         val name:TextView=itemView.findViewById(R.id.name)
+        val main:ConstraintLayout=itemView.findViewById(R.id.main)
+        init {
+
+            itemView.setOnClickListener {
+                currentSelect=layoutPosition
+                notifyDataSetChanged()
+            }
+        }
     }
 
 
