bb5a6e997807759066fb6c74d50123934d3b3f07
liguanghao <liguanghao@viatomtech.com>
2022-08-15 11:03:07 +0800

    bnmfhgdffggfsdgdfsgfdgdfgagfghfsdfxf

--- app/src/main/java/com/vaca/pc300/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/vaca/pc300/ui/history/HistoryFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.vaca.pc300.R
 import com.vaca.pc300.databinding.FragmentHistoryBinding
 import com.vaca.pc300.ui.dashboard.adapter.PC300DataDetailAdapter
 import com.vaca.pc300.ui.dashboard.adapter.SpaceItemDecoration3
@@ -17,12 +21,21 @@ import com.vaca.pc300.ui.history.adapter.PC300HistoryLeftAdapter
 class HistoryFragment : Fragment() {
 
     private var _binding: FragmentHistoryBinding? = null
-
+    companion object {
+        val initJump = MutableLiveData<Int>()
+    }
 
     private val binding get() = _binding!!
-
+    var currentIndex = 0
+    lateinit var navController: NavController
     private lateinit var leftAdapter: PC300HistoryLeftAdapter
-
+    val topId = arrayOf(
+        R.id.PC300HistoryBPFragment,
+        R.id.PC300HistorySPO2Fragment,
+        R.id.PC300HistoryTempFragment,
+        R.id.PC300HistoryGluFragment,
+        R.id.PC300HistoryECGFragment,
+    )
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -43,6 +56,18 @@ class HistoryFragment : Fragment() {
         }
         binding.leftView.adapter =leftAdapter
 
+
+        initJump.postValue(0)
+
+
+        val fm = childFragmentManager.findFragmentById(R.id.bx) as NavHostFragment
+        navController = fm.navController
+        val graph = navController.navInflater.inflate(R.navigation.pc300_history_navigation)
+        initJump.observe(viewLifecycleOwner) {
+            currentIndex = it
+            graph.setStartDestination(topId[it])
+            navController.graph = graph
+        }
 
 
         return root
---
+++ app/src/main/java/com/vaca/pc300/ui/history/fragment/PC300HistoryECGFragment.kt
@@ -0,0 +1,55 @@
+package com.vaca.pc300.ui.history.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vaca.pc300.databinding.FragmentHistoryBinding
+import com.vaca.pc300.databinding.FragmentHistoryEcgBinding
+import com.vaca.pc300.ui.dashboard.adapter.PC300DataDetailAdapter
+import com.vaca.pc300.ui.dashboard.adapter.SpaceItemDecoration3
+import com.vaca.pc300.ui.history.HistoryViewModel
+import com.vaca.pc300.ui.history.adapter.PC300HistoryLeftAdapter
+
+class PC300HistoryECGFragment : Fragment() {
+
+    private var _binding: FragmentHistoryEcgBinding? = null
+
+
+    private val binding get() = _binding!!
+
+    private lateinit var leftAdapter: PC300HistoryLeftAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHistoryEcgBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        leftAdapter= PC300HistoryLeftAdapter(requireContext())
+
+        binding.listView.layoutManager = object :  LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false) {
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
+        binding.listView.adapter =leftAdapter
+
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
