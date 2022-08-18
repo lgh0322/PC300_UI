f82491e009bf4e8dfa31887cfa533deeea09c623
liguanghao <liguanghao@viatomtech.com>
2022-08-18 17:20:55 +0800

    dfhasdfcvbsdfsdgdfsdfggxfcgfdxcgxgfdgxcgdfghfhdfgfvbvbdfg

--- app/src/main/java/com/vaca/pc300/room/PCdata.kt
+++ app/src/main/java/com/vaca/pc300/room/PCdata.kt
@@ -6,19 +6,19 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class PCdata(
-    @PrimaryKey var date: Long=0L,
-    @ColumnInfo(name = "dateString") var dateString: String="",
-    @ColumnInfo(name = "id") var id: String="",
-    @ColumnInfo(name = "type") var type: Int=0,
-    @ColumnInfo(name = "sys") var sys: Int=0,
-    @ColumnInfo(name = "dia") var dia: Int=0,
-    @ColumnInfo(name = "pr") var pr: Int=0,
-    @ColumnInfo(name = "o2") var o2: Int=0,
-    @ColumnInfo(name = "temp") var temp:Float=0f,
-    @ColumnInfo(name = "glu") var glu:Float=0f,
-    @ColumnInfo(name = "hr") var hr: Int=0,
-    @ColumnInfo(name = "ecg_data") var ecg_data: DoubleArray?=null,
-    @ColumnInfo(name = "ecg_result") var ecg_result: String="",
-    @ColumnInfo(name = "note") var note: String="",
+    @PrimaryKey var date: Long = 0L,
+    @ColumnInfo(name = "dateString") var dateString: String = "",
+    @ColumnInfo(name = "id") var id: String = "",
+    @ColumnInfo(name = "type") var type: Int = 0,
+    @ColumnInfo(name = "sys") var sys: Int = 0,
+    @ColumnInfo(name = "dia") var dia: Int = 0,
+    @ColumnInfo(name = "pr") var pr: Int = 0,
+    @ColumnInfo(name = "o2") var o2: Int = 0,
+    @ColumnInfo(name = "temp") var temp: Float = 0f,
+    @ColumnInfo(name = "glu") var glu: Float = 0f,
+    @ColumnInfo(name = "hr") var hr: Int = 0,
+    @ColumnInfo(name = "ecg_data") var ecg_data: DoubleArray? = null,
+    @ColumnInfo(name = "ecg_result") var ecg_result: String = "",
+    @ColumnInfo(name = "note") var note: String = "",
 
     )
