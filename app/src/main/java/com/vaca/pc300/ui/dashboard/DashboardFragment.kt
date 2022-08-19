e2897b20e5b9fb5eb07b462b4ad0cce436d015a1
liguanghao <liguanghao@viatomtech.com>
2022-08-19 16:18:13 +0800

    dfhgd66serdfgfxbdfhsdggfh1566dfghdfrtfghrgfh5

--- app/src/main/java/com/vaca/pc300/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/vaca/pc300/ui/dashboard/DashboardFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bigkoo.convenientbanner.ConvenientBanner
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator
+import com.bigkoo.convenientbanner.listener.OnPageChangeListener
+import com.vaca.pc300.R
 import com.vaca.pc300.databinding.FragmentDashboardBinding
-import com.vaca.pc300.ui.dashboard.adapter.LPMRealTimeDataAdapter
-import com.vaca.pc300.ui.dashboard.adapter.PC300DataDetailAdapter
-import com.vaca.pc300.ui.dashboard.adapter.SpaceItemDecoration3
-import com.vaca.pc300.ui.dashboard.adapter.SpaceItemDecorationLPM
+import com.vaca.pc300.ui.dashboard.adapter.*
 
 class DashboardFragment : Fragment() {
 
@@ -21,9 +24,65 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private lateinit var dataAdapter: LPMRealTimeDataAdapter
+
+    private fun initHomeImg() {
+
+        val con: ConvenientBanner<BannerBean> = binding.banner as ConvenientBanner<BannerBean>
+
+        val bannerList = listOf<BannerBean>(BannerBean(0), BannerBean(1), BannerBean(2), BannerBean(3))
+        val mDotsLayout = binding.dotLayout
+        con.setPages(
+            object : CBViewHolderCreator {
+                override fun createHolder(itemView: View): BannerHolder {
+                    return BannerHolder(itemView)
+                }
+
+                override fun getLayoutId(): Int {
+                    return R.layout.item_banner
+                }
+            }, bannerList
+        )
+            .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+            .setOnPageChangeListener(object : OnPageChangeListener {
+                override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                    if (newState == 0) {
+                        for (k in 0 until 4) {
+                            mDotsLayout.getChildAt(k)?.isSelected = k == con.currentItem
+                        }
+
+                    }
+                }
+
+                override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+
+                }
+
+                override fun onPageSelected(index: Int) {
+
+                }
+
+            })
+
+        mDotsLayout.removeAllViews()
+        for (k in 0 until bannerList.size) {
+            val params = ViewGroup.MarginLayoutParams(20, 20)
+            params.leftMargin = 20
+            params.rightMargin = 20
+            mDotsLayout.addView(dotsItem(k), params)
+        }
+        mDotsLayout.getChildAt(0)?.isSelected = true
 
 
+    }
+
+    private fun dotsItem(position: Int): ImageView {
+        val inflater =
+            requireContext().getSystemService(AppCompatActivity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val layout = inflater.inflate(R.layout.home_dot_image, null)
+        val iv = layout.findViewById<View>(R.id.face_dot) as ImageView
+        iv.id = position
+        return iv
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -44,16 +103,7 @@ class DashboardFragment : Fragment() {
 
 
 
-      /*  dataAdapter= LPMRealTimeDataAdapter(requireContext())
-
-
-        binding.leftView.layoutManager = object : GridLayoutManager(requireContext(), 2) {
-            override fun canScrollVertically(): Boolean {
-                return false
-            }
-        }
-        binding.leftView.adapter =dataAdapter
-        binding.leftView.addItemDecoration(SpaceItemDecorationLPM(50))*/
+        initHomeImg()
 
         return root
     }
