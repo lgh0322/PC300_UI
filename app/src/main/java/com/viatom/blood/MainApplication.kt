e40086bfed921ce65911e987b0efa2844ecd3793
liguanghao <liguanghao@viatomtech.com>
2022-08-24 05:55:23 +0800

    sdfdsfdfsdfdsfsdsdffsdfasdfasdf

---
+++ app/src/main/java/com/viatom/blood/MainApplication.kt
@@ -0,0 +1,37 @@
+package com.viatom.blood
+
+
+import android.app.Application
+import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.Observer
+import com.jeremyliao.liveeventbus.LiveEventBus
+import com.lepu.blepro.BleServiceHelper
+import com.lepu.blepro.ble.cmd.Pc300BleResponse
+import com.lepu.blepro.event.InterfaceEvent
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+
+class MainApplication : Application() {
+
+    companion object {
+        val dataScope = CoroutineScope(Dispatchers.IO)
+        lateinit var application: Application
+    }
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+        application = this
+        BleServiceHelper.BleServiceHelper.initService(this, null)
+
+
+
+
+    }
+
+
+}
