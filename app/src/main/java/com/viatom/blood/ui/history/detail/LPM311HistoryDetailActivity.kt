b7d93e855a7eb1cb3eaca758592deeb6b39354fe
liguanghao <liguanghao@viatomtech.com>
2022-08-24 07:05:19 +0800

    sdgsdfsdaf

--- app/src/main/java/com/viatom/blood/ui/history/detail/LPM311HistoryDetailActivity.kt
+++ app/src/main/java/com/viatom/blood/ui/history/detail/LPM311HistoryDetailActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.viatom.blood.databinding.ActivityLpm311HistoryDetailBinding
 import com.viatom.blood.room.LPM311AppDatabase
 import com.viatom.blood.ui.history.LPM311HistoryFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LPM311HistoryDetailActivity : AppCompatActivity() {
 
@@ -17,6 +20,22 @@ class LPM311HistoryDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.back.setOnClickListener {
             onBackPressed()
+        }
+
+        LPM311HistoryFragment.currentSelect.observe(this){
+            binding.time.text=it.dateString
+            binding.chol.text=it.chol.toString()
+            binding.hdl.text=it.hdl.toString()
+            binding.ldl.text=it.ldl.toString()
+            binding.cholHdl.text=it.cholhdl.toString()
+            binding.trig.text=it.trig.toString()
+
+            LPM311AppDatabase.dataScope.launch {
+                val note= LPM311AppDatabase.lpmDb.lpmDao().getNote(it.date)
+                withContext(Dispatchers.Main){
+                    binding.note.setText(note)
+                }
+            }
         }
     }
 
