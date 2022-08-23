0e40ac0c7266c479aea2b4464d41db8ab6e8fb7e
liguanghao <liguanghao@viatomtech.com>
2022-08-24 05:47:05 +0800

    sdfdsfdfsdfdsfsdf

--- app/src/main/java/com/viatom/blood/LPM311MainActivity.kt
+++ app/src/main/java/com/viatom/blood/LPM311MainActivity.kt
@@ -5,16 +5,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.viatom.blood.databinding.ActivityMainBinding
+import com.viatom.blood.databinding.Lpm311ActivityMainBinding
 
 class LPM311MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: Lpm311ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = Lpm311ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
