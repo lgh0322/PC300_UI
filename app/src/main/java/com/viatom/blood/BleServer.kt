86f52041022c7af25a4807f97d13ac34f95807a9
liguanghao <liguanghao@viatomtech.com>
2022-08-28 12:22:46 +0800

    sdgsdfsdafgfjhasdfdsaffdsg25564cgvb

---
+++ app/src/main/java/com/viatom/blood/BleServer.kt
@@ -0,0 +1,9 @@
+package com.viatom.blood
+
+import androidx.lifecycle.MutableLiveData
+
+object BleServer {
+
+    val wifi = MutableLiveData<Boolean>()
+
+}
--- app/src/main/java/com/viatom/blood/LPM311MainActivity.kt
+++ app/src/main/java/com/viatom/blood/LPM311MainActivity.kt
@@ -1,6 +1,9 @@
 package com.viatom.blood
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.text.format.Formatter
@@ -11,11 +14,14 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.viatom.blood.databinding.Lpm311ActivityMainBinding
-
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 
 class LPM311MainActivity : AppCompatActivity() {
-
+    private val networkManager by lazy {
+        getSystemService(AppCompatActivity.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
     private lateinit var binding: Lpm311ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +37,12 @@ class LPM311MainActivity : AppCompatActivity() {
 
 
 
-       val a=getWifiRouteIPAddress(this);
-        Log.e("gaga",a)
+
+
+        val builder = NetworkRequest.Builder()
+        val request = builder.build()
+        networkManager.registerNetworkCallback(request, networkCallback)
+
 
 
     }
@@ -43,5 +53,36 @@ class LPM311MainActivity : AppCompatActivity() {
         val dhcpInfo = wifi_service.dhcpInfo
         val routeIp: String = Formatter.formatIpAddress(dhcpInfo.gateway)
         return routeIp
+    }
+
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            val a=getWifiRouteIPAddress(MainApplication.application);
+            Log.e("gaga",a)
+            MainScope().launch {
+                BleServer.wifi.postValue(true)
+            }
+
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            MainScope().launch {
+                BleServer.wifi.postValue(false)
+            }
+
+        }
+
+    }
+
+
+
+
+    override fun onDestroy() {
+        networkManager.unregisterNetworkCallback(networkCallback)
+        super.onDestroy()
+
     }
 }
